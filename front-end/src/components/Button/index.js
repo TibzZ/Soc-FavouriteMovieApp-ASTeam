3836cf4da092593849957931c8bf6ce988a55ec3
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-19 15:52:10 +0000

    tailwind added

--- front-end/src/components/Button/index.js
+++ front-end/src/components/Button/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Button = ({name, onClick}) => {
-    return (
-        <div>
-            <button  onClick={onClick}> {name} </button>
-        </div>
-    );
+const Button = ({ name, onClick }) => {
+	return (
+		<div>
+			<button
+				class='bg-green-600 rounded-lg border-4 border-green-800 p-2 inline-block font-serif  text-3xl tracking-wide transition delay-150 duration-300 ease-in-out'
+				onClick={onClick}
+			>
+				{' '}
+				{name}{' '}
+			</button>
+		</div>
+	);
 };
 
 export default Button;
