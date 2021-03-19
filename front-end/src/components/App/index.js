f806b3efbc706abda21dc9a81ca278cc4fd07ed2
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-19 15:33:48 +0000

    merge

--- front-end/src/components/App/index.js
+++ front-end/src/components/App/index.js
@@ -9,31 +9,29 @@ import './App.css';
 
 function App() {
 	const [ search, setSearch ] = useState('');
-	//const [data, setData] = useState("");
-	const { user, isAuthenticated, isLoading } = useAuth0();
+
+	const { logout, isAuthenticated } = useAuth0();
 
 	const data = useGetMovies(search);
 
 	console.log('from the App:');
 	console.log(data);
 
-	function onClick() {
-		throw Error();
-	}
-
 	return (
 		<div>
 			{isAuthenticated && (
 				<div className='App'>
 					{' '}
-					<Button name={'Add Movie'} onClick={onClick} />
-					<Button name={'Save Movie'} onClick={onClick} />
+					<Button name="Log Out" onClick={logout}/>
 					<SearchInput setValue={setSearch} />
 					<FilmList data={data} />
 				</div>
 			)}
-
-			<HomePage />
+      {
+        !isAuthenticated && (
+          <HomePage />
+        )
+      }
 		</div>
 	);
 }
