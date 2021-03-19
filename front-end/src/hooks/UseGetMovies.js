2f66b0184babeacdf317112daacff2da914052b0
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-19 12:42:45 +0000

    useGetMovies updated useEffect now works

--- front-end/src/hooks/UseGetMovies.js
+++ front-end/src/hooks/UseGetMovies.js
@@ -4,21 +4,25 @@ async function useGetMovies(search) {
     const searchString = search.replace(" ", "%20");
     const [data, setData] = useState(null);
 
-    function getData(){
-        const res = await fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${searchString}`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": process.env.REACT_APP_X_RAPID_API_KEY,
-                "x-rapidapi-host": process.env.REACT_APP_X_RAPID_API_HOST
+    async function getData(){
+        if (data === null) {
+            const res = await fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${searchString}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": process.env.REACT_APP_X_RAPID_API_KEY,
+                    "x-rapidapi-host": process.env.REACT_APP_X_RAPID_API_HOST
+                }
             }
-        }
-        )
-        const resData = await res.json();
-        
-        setData(resData)
+            )
+            const resData = await res.json();
+            
+            setData(resData)
         }
 
-    return data;
+        }
+        useEffect(() => getData(), [search]);
+    
+        return data;
 }
 
 
